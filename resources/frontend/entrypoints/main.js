082cc4bd8ea5f682908f228c84d49302d7a5f2fb
Julio Rodas <jrrmcalcio@gmail.com>
2022-08-24 15:29:28 -0600

    feat(chore): adds tailwindcss to the project
    adds tailwindcss to the project along with a cspell.json config file

--- resources/frontend/entrypoints/main.js
+++ resources/frontend/entrypoints/main.js
@@ -1,33 +1,32 @@
-import {createApp, h} from 'vue';
-import {createInertiaApp, Head, Link} from '@inertiajs/inertia-vue3';
-import {InertiaProgress} from "@inertiajs/progress";
-import ElementPlus from 'element-plus';
-import 'element-plus/dist/index.css';
+import { createInertiaApp, Head, Link } from '@inertiajs/inertia-vue3'
+import { InertiaProgress } from '@inertiajs/progress'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+import { createApp, h } from 'vue'
+import '../css/app.css'
 
 export function resolvePageComponent(name, pages) {
-  for (const path in pages) {
-    if (path.endsWith(`${name.replace('.', '/')}.vue`)) {
-      return typeof pages[path] === 'function'
-        ? pages[path]()
-        : pages[path]
-    }
-  }
+	for (const path in pages) {
+		if (path.endsWith(`${name.replace('.', '/')}.vue`)) {
+			return typeof pages[path] === 'function' ? pages[path]() : pages[path]
+		}
+	}
 
-  throw new Error(`Page not found: ${name}`)
+	throw new Error(`Page not found: ${name}`)
 }
 
 createInertiaApp({
-  resolve(name) {
-    return resolvePageComponent(name, import.meta.glob("../Pages/**/*.vue"))
-  },
-  setup({el, App, props, plugin}) {
-    createApp({ render: () => h(App, props)})
-      .use(plugin)
-      .use(ElementPlus)
-      .component('InertiaHead', Head)
-      .component('InertiaLink', Link)
-      .mount(el)
-  }
+	resolve(name) {
+		return resolvePageComponent(name, import.meta.glob('../Pages/**/*.vue'))
+	},
+	setup({ el, App, props, plugin }) {
+		createApp({ render: () => h(App, props) })
+			.use(plugin)
+			.use(ElementPlus)
+			.component('InertiaHead', Head)
+			.component('InertiaLink', Link)
+			.mount(el)
+	},
 })
 
-InertiaProgress.init();
+InertiaProgress.init()
---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ['./index.html', './resources/views/**/*.{vue,js,ts,jsx,tsx}', './resources/frontend/Pages/**/*.{vue,js,ts,jsx,tsx}'],
+	theme: {
+		extend: {},
+	},
+	plugins: [],
+}
